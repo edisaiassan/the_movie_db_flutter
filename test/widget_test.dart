24ccacc21c99977512fede038029491dd9ce749d
Edson Sanchez Chota <esanchez@devcloud.pe>
2023-07-06 11:10:23 -0500

    Estructura Principal, add directorios Clean Arquitecture

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:the_movie_db/main.dart';
+import 'package:the_movie_db/src/my_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
