0544eb780a1c4e19a4a340c9aa4a48c6322cb724
Edson <edisaiassan@gmail.com>
2023-11-04 16:41:50 -0500

    logs

--- lib/src/data/http/http.dart
+++ lib/src/data/http/http.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
 import 'dart:io';
+import 'package:flutter/foundation.dart';
 import 'package:http/http.dart';
 import 'package:the_movie_db/src/domain/either.dart';
+import 'dart:developer';
 
 class Http {
   final Client _client;
@@ -18,41 +20,42 @@ class Http {
 
   Future<Either<HttpFailure, R>> request<R>(
     String path, {
-      required Function(String responseBody) onSucces,
+    required Function(String responseBody) onSucces,
     HttpMethod method = HttpMethod.get,
     Map<String, String> headers = const {},
     Map<String, String> queryParameters = const {},
     Map<String, dynamic> body = const {},
     bool useApiKey = true,
   }) async {
+    Map<String, dynamic> logs = {};
+    StackTrace? stackTrace;
 
     try {
       if (useApiKey) {
-        queryParameters = {
-          ...queryParameters,
-          'api_key': _apiKey
-        };
+        queryParameters = {...queryParameters, 'api_key': _apiKey};
       }
 
       Uri url = Uri.parse(path.startsWith('http') ? path : '$_baseUrl$path');
       if (queryParameters.isNotEmpty) {
         url = url.replace(
-          //lo que hace replace es hacer una copia de la variable url
-          //y agregarle, en este caso, queryParameters
           queryParameters: queryParameters,
         );
       }
 
       headers = {
-        'Content-Type':
-            'application/json', //Todas las colicitudes contienen esto
+        'Content-Type': 'application/json',
         ...headers,
       };
 
       late final Response response;
       final bodyString = jsonEncode(body);
 
-      //Parte de métodos
+      logs = {
+        'url': url.toString(),
+        'method': logs['method'] = method.name,
+        'body': logs['body'] = body,
+      };
+
       switch (method) {
         case HttpMethod.get:
           response = await _client.get(url);
@@ -88,31 +91,64 @@ class Http {
       }
 
       final statusCode = response.statusCode;
+
+      logs = {
+        ...logs,
+        'start_time': DateTime.now().toString(),
+        'status_code': statusCode,
+        'response_body': response.body,
+      };
       if (statusCode >= 200 && statusCode < 300) {
         return Either.right(onSucces(response.body));
       }
       return Either.left(
-        //Condición de solicitud fallida
         HttpFailure(
           statusCode: statusCode,
         ),
       );
-    } catch (e) {
+    } catch (e, s) {
+      stackTrace = s;
+      logs = {
+        ...logs,
+        'exception': e.runtimeType,
+      };
       if (e is SocketException || e is ClientException) {
+        logs = {
+          ...logs,
+          'exception': 'NetworkException',
+        };
         return Either.left(
           HttpFailure(
             exception: NetworkException(),
           ),
         );
       }
-      return Either.left(HttpFailure(exception: e));
+
+      return Either.left(
+        HttpFailure(
+          exception: e,
+        ),
+      );
+    } finally {
+      logs = {...logs, 'end_time': DateTime.now().toString()};
+      if (kDebugMode) {
+        log('''
+😊
+------------------------
+${const JsonEncoder.withIndent(' ').convert(logs)}
+--------------------
+😊
+''',
+stackTrace: stackTrace,
+);
+      }
     }
   }
 }
 
 class HttpFailure {
-  final int? statusCode; //Para recibir el statusCode de la conexión
-  final Object? exception; //Para recibir errores de conexión
+  final int? statusCode;
+  final Object? exception;
 
   HttpFailure({this.statusCode, this.exception});
 }
